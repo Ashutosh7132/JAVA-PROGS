2a147c3508cc0fd624f1632e491f905239ce0b3e
ashutosh7131 <ashutosh713@outlook.com>
2024-01-21 23:00:47 +0800

    LL printlist method

---
+++ src/CustomLL.java
@@ -0,0 +1,13 @@
+public class CustomLL {
+
+    public static void main(String[] args) {
+
+        LinkedList linkedList = new LinkedList(4);
+
+        linkedList.getHead();
+        linkedList.getTail();
+        linkedList.getLength();
+        linkedList.printList();
+
+    }
+}
