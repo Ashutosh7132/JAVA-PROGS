735db72882787fef366464eaba1088a532a1dcfb
ashutosh7131 <ashutosh713@outlook.com>
2024-01-21 23:52:40 +0800

    LL Append Operation commit

--- DataStructure/CustomLL.java
+++ DataStructure/CustomLL.java
@@ -2,11 +2,12 @@ public class CustomLL {
 
     public static void main(String[] args) {
 
-        LinkedList linkedList = new LinkedList(4);
+        LinkedList linkedList = new LinkedList(1);
 
         linkedList.getHead();
         linkedList.getTail();
         linkedList.getLength();
+        linkedList.append(2);
         linkedList.printList();
 
     }
